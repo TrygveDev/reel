4cfc2ab256df5a79ae367f695b2ab2e29486f3e4
Trygve <trygvemf@gmail.com>
2023-05-09 14:29:43 +0200

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./libs/firebase";
+
 export default function Home() {
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			console.log("user is logged in");
+		} else {
+			console.log("user is not logged in");
+		}
+	});
 	return <main>Dekan</main>;
 }
